d11ce62626e209f91267a311e1818bd388e9b421
Nikiponce <niki_ponce@hotmail.com>
2016-07-12 15:09:58 +0200

    avances

---
+++ MyApplication/app/src/main/java/com/example/np11/myapplication/Alumno.java
@@ -0,0 +1,14 @@
+package com.example.np11.myapplication;
+
+/**
+ * Created by NP11 on 12/07/2016.
+ */
+public class Alumno extends Persona {
+
+    private int calificacion;
+
+    public Alumno (int calificacion)
+    {
+        this.calificacion = calificacion;
+    }
+}
