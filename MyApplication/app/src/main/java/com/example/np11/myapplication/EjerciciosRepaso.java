32a803327c88f61a8cce039c06e31bee8995f3d0
Niki <n.ponce@everyoneplus.es>
2016-07-14 09:19:58 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #
    MyApplication/app/src/main/java/com/example/np11/myapplication/Ejercicio
    sRepaso.java
    #
    MyApplication/app/src/main/java/com/example/np11/myapplication/MainActiv
    ity.java
    #	MyApplication/app/src/main/res/layout/activity_main.xml
    #	MyApplication/app/src/main/res/mipmap-hdpi/ic_launcher.png
    #	MyApplication/app/src/main/res/mipmap-mdpi/ic_launcher.png
    #	MyApplication/app/src/main/res/mipmap-xhdpi/ic_launcher.png
    #	MyApplication/app/src/main/res/mipmap-xxhdpi/ic_launcher.png
    #	MyApplication/app/src/main/res/mipmap-xxxhdpi/ic_launcher.png

--- MyApplication/app/src/main/java/com/example/np11/myapplication/EjerciciosRepaso.java
+++ MyApplication/app/src/main/java/com/example/np11/myapplication/EjerciciosRepaso.java
@@ -1,5 +1,6 @@
 package com.example.np11.myapplication;
 
+<<<<<<< HEAD
 
 /**
  * Created by g5 on 13/7/16.
@@ -24,10 +25,91 @@ public class EjerciciosRepaso {
 
             auxiliar = palabra.charAt(i);
             if (auxiliar == letra){
+=======
+/**
+ * Created by NP11 on 13/07/2016.
+ */
+public class EjerciciosRepaso {
+
+    //    1) HACER UN MÉTODO QUE RECIBA UNA CADENA Y UN CARACTER Y DIGA SI ESE CARACTER PERTENECE A LA CADENA O NO
+    public boolean estaLetraPalabra(String palabra, char letra) {
+        boolean res = false;
+        if (palabra.indexOf(letra) > -1) {
+            res = true;
+        }
+        return res;
+    }
+
+    //    2) HACER UN MÉTODO QUE RECIBA UNA CADENA Y UN CARACTER Y DIGA CÚANTAS VECES APARECE ESE CARACTER EN LA CADENA
+    public int vecesLetraEnPalabra(String palabra, char letra) {
+        int n_veces = 0;
+
+        for (int i = 0; i < palabra.length(); i++) {
+            if (palabra.charAt(i) == letra) {
+>>>>>>> origin/master
                 n_veces++;
             }
         }
         return n_veces;
     }
 
+<<<<<<< HEAD
+=======
+    //    3) HACER UN MÉTODO QUE DADO UN NÚMERO, DIGA SI ES PAR O NO
+    public String numeroPar(int numero) {
+        String res = "IMPAR";
+        if (numero % 2 == 0) {
+            res = "PAR";
+        }
+        return res;
+    }
+
+    //    5) HACER UN MÉTODO QUE RECIBA UNA NOTA DE 0 A 10 Y DIGA SI EQUIVALE A UN APROBADO, BIEN, NOTABLE, O SOBRESALIENTE
+    public String calificar(double nota) {
+        String calificacion = "";
+        if (nota == 10) {
+            calificacion = "Matrícula de Honor";
+        } else {
+            if (nota <= 9.9 && nota >= 9) {
+                calificacion = "Sobresaliente";
+            } else {
+                if (nota <= 8.9 && nota >= 7) {
+                    calificacion = "Notable";
+                } else {
+                    if (nota <= 6.9 && nota >= 5) {
+                        calificacion = "Aprobado";
+                    } else {
+                        calificacion = "Suspenso";
+                    }
+                }
+            }
+        }
+        return calificacion;
+    }
+
+    //    6) HACER UN MÉTODO QUE RECIBA UNA CADENA Y LA DEVUELVA ALREVÉS
+    public String palabraDelReves(String palabra) {
+        String res = "";
+//        for (int i = palabra.length();  i > -1; i--) {
+//            res = res + palabra.charAt(i);
+//        }
+        for (int i = 0; i < palabra.length(); i++) {
+            res = palabra.charAt(i) + res;
+        }
+        return res;
+    }
+
+    //    7) HACER UN PROGRAMA QUE MUESTRE LA SECUENCIA 3, 6, 9, 12, 15 ...99
+    public String secuenciaDe3En3() {
+        String res = "";
+        for (int i = 3; i <= 99; i = i + 3) {
+            if (i == 99) {
+                res = res + i;
+            } else {
+                res = res + i + ", ";
+            }
+        }
+        return res;
+    }
+>>>>>>> origin/master
 }
--- MyApplication/app/src/main/java/com/example/np11/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/np11/myapplication/MainActivity.java
@@ -30,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
 
         Alumno alumno2 = (Alumno)persona_alumno;//Infracasting
 
+<<<<<<< HEAD
 
 
         EjerciciosRepaso ejercicio = new EjerciciosRepaso();
@@ -41,6 +42,68 @@ public class MainActivity extends AppCompatActivity {
         }
 
         Log.d(getClass().getCanonicalName(), "La letra aparece "+ ejercicio.vecesLetrasEnPalabra("cadena", 'a') +" veces");
+=======
+        if (alumno instanceof Persona)
+        {
+            // Alumno es una persona
+            Log.d (getClass().getCanonicalName(), "El alumno es una persona!!");
+
+            String nombre_alumno = alumno.obtenerNombre();
+
+            if (nombre_alumno != null )
+            {
+                Log.d(getClass().getCanonicalName(), "El alumno no es nulo");
+            }
+            else
+            {
+                Log.d(getClass().getCanonicalName(), "El alumno es nulo");
+            }
+/*
+            try {
+                Log.d(getClass().getCanonicalName(), " = "+ nombre_alumno.length());
+            } catch (Throwable t){
+                Log.e(getClass().getCanonicalName(), "ERROR",t);
+            }
+*/
+        }
+        else {
+            // el alumno no es una persona
+            Log.d (getClass().getCanonicalName(), "El alumno NO es una persona!!");
+        }
+
+        EjerciciosRepaso ejerciciosRepaso = new EjerciciosRepaso();
+
+//    1) HACER UN MÉTODO QUE RECIBA UNA CADENA Y UN CARACTER Y DIGA SI ESE CARACTER PERTENECE A LA CADENA O NO
+        boolean resultado = ejerciciosRepaso.estaLetraPalabra("NIKI", 'I');
+        if (resultado) {
+            Log.d(getClass().getCanonicalName(), "Si que está!!!");
+        } else {
+            Log.d(getClass().getCanonicalName(), "Que va, no esta!!!");
+        }
+
+//    2) HACER UN MÉTODO QUE RECIBA UNA CADENA Y UN CARACTER Y DIGA CÚANTAS VECES APARECE ESE CARACTER EN LA CADENA
+        int numero = ejerciciosRepaso.vecesLetraEnPalabra("esta es una oración", 'x');
+        if (numero > 0){
+            Log.d(getClass().getCanonicalName(), "La letra se repite "+ numero +" veces");
+        } else {
+            Log.d(getClass().getCanonicalName(), "La letra no aparece en la palabra");
+        }
+
+//    3) HACER UN MÉTODO QUE DADO UN NÚMERO, DIGA SI ES PAR O
+        Log.d(getClass().getCanonicalName(), "El número es "+ ejerciciosRepaso.numeroPar(7));
+
+//    5) HACER UN MÉTODO QUE RECIBA UNA NOTA DE 0 A 10 Y DIGA SI EQUIVALE A UN APROBADO, BIEN, NOTABLE, O SOBRESALIENTE
+        Log.d(getClass().getCanonicalName(), "La calificación es: "+ ejerciciosRepaso.calificar(6.5));
+
+//    6) HACER UN MÉTODO QUE RECIBA UNA CADENA Y LA DEVUELVA ALREVÉS
+        String cadena = "Palabrota";
+        String mensaje = cadena+" se escribe así "+ejerciciosRepaso.palabraDelReves(cadena)+" del revés";
+        Log.d(getClass().getCanonicalName(), "La palabra "+ mensaje);
+
+//    7) HACER UN PROGRAMA QUE MUESTRE LA SECUENCIA 3, 6, 9, 12, 15 ...99
+        Log.d(getClass().getCanonicalName(), "La secuencia de número de 3 en 3 hasta el 99 es: " + ejerciciosRepaso.secuenciaDe3En3());
+
+>>>>>>> origin/master
     }
 
 }
